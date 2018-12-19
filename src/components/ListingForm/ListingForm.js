dd2464c3a02e3a3cad82511b4bb04d18057653a3
Colin White <clwhite814@gmail.com>
2018-12-18 19:18:34 -0600

    added listing form

--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -5,7 +5,19 @@ class ListingForm extends React.Component {
   render() {
     return (
       <div className="listingForm col">
-        <h2>Listing Forms</h2>
+        <h2>Add New Listing:</h2>
+        <form>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Address</label>
+              <input
+              type="text"
+              className="form-control"
+              id="address"
+              aria-describedby="addressHelp"
+              placeholder="123 Main Street, Nashville, TN 37221" />
+            </div>
+            <button className="btn btn-danger">Save Listing</button>
+            </form>
       </div>
     );
   }
